e16973fa029f6156843939020cfd85f7f6e35016
Unknown <svampodling@gmail.com>
2018-10-03 03:43:42 +0200

    Styling

--- MediaPlayer/src/application/Main.java
+++ MediaPlayer/src/application/Main.java
@@ -1,13 +1,10 @@
 package application;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
